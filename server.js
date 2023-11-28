511cec232178b47876e15437abc02275f16f9bff
sudeesh <sudeeshbabu2000@gmail.com>
2023-11-28 11:33:41 +0530

    add hash and hash compare functions

--- server.js
+++ server.js
@@ -1,56 +1,30 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
-const bcrypt      = require('bcrypt');  
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const app = express();
+const bcrypt = require("bcrypt");
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+app.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.hash(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
 
-
-
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {});
